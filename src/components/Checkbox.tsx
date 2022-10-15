980c6b77cec2efe449bd3fddb4daae6a3f0e6ba2
Ivan Seibel <me@ivanseibel.dev>
2022-10-15 12:39:08 +0100

    Extend interface to receive classes

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,24 +1,28 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+import clsx from 'clsx'
 import { Check } from 'phosphor-react'
 import { Text } from './Text'
 
-export interface CheckboxProps extends React.ForwardRefExoticComponent<CheckboxPrimitive.CheckboxProps & React.RefAttributes<HTMLButtonElement>>{
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {
   label: string
+  labelClassName?: string
+  labelSize?: 'sm' | 'md' | 'lg'
 }
 
-export function Checkbox({ label }: CheckboxProps) {
+export function Checkbox({ label, labelClassName, labelSize = 'sm', id }: CheckboxProps) {
   return (
     <div className="flex items-center gap-2">
       <CheckboxPrimitive.Root 
         className='w-6 h-6 p-[2px] bg-gray-800 rounded' 
         aria-label={label}
+        id={id}
       >
         <CheckboxPrimitive.Indicator asChild>
           <Check className="h-5 w-5 text-cyan-500" weight='bold' />
         </CheckboxPrimitive.Indicator>
       </CheckboxPrimitive.Root>
       
-      <Text size='sm'>
+      <Text size='sm' className={clsx(labelClassName)}>
         {label}
       </Text>
     </div>
